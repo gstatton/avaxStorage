2184d36ae2842d665f1dca84dddad6aab153875e
gstatton <gstatton@users.noreply.github.com>
2022-06-19 15:36:19 -0700

    updating app.js and IPFS Viewer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 
 // 1. Importing other modules
 
+import {GetWeb3, GetContract, GetAccount} from './BlockchainUtil';
+import IPFSUploader from './IPFSUploader';
+import IPFSViewer from './IPFSViewer';
+
+import contractJson from './build/contracts/FileManager.json';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -18,19 +24,35 @@ class App extends React.Component {
 
   async init() {
     // 2. Load web3
+
+    const Web3 = new GetWeb3();
+    this.web3 = await Web3.getWeb3();
+    this.setState({web3: this.web3});
     // 3. Load Account
+    const Account = new GetAccount();
+    this.account = await Account.getAccount(this.web3);
+    this.setState({account: this.account[0]});
     // 4. Load Smart-Contract instance
+    const Contract = new GetContract();
+    this.contract = await Contract.getContract(this.web3, contractJson);
+    this.setState({contract: this.contract});
   }
 
   render() {
     return (
       <div>
         <font color="white">Distributed File Manager</font>
-        {/* 5. Navbar */}
+          {/* 5. Navbar */}
+            <nav className="navbar navbar-dark shadow" style={{backgroundColor: "#1b2021", height: "60px", color: "white"}}>
+            <b>Distributed File Manager</b>
+            <span style={{float: "right"}}>{this.state.account}</span>
+          </nav>
 
-        {/* 6. IPFS Viewer component */}
+{/* 6. IPFS Viewer com  ponent */}
+<IPFSViewer state = {this.state} />
 
-        {/* 7. IPFS Uploader component */}
+{/* 7. IPFS Uploader component */}
+<IPFSUploader state = {this.state} />
       </div>
     );
   }
